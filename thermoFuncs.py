ccbf83c6dbb149e19e2247eb07218e044a75b1ab
burtasun <benaturtasun@gmail.com>
2024-04-10 10:00:15 +0200

    First Commit

---
+++ thermoFuncs.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib as mlp
+
+import cv2 as cv
+
+if __name__=='__main__':
+    print('''
+--------------------------------------------
+    Thermographic processing algorithms
+--------------------------------------------
+''')
