896fe8d491ee23cb094e8e9f76f42219d2161c6d
burtasun <benaturtasun@gmail.com>
2024-04-29 16:05:17 +0200

    pars input

--- globalVars.py
+++ globalVars.py
@@ -2,3 +2,13 @@
 class Params:
     outputDir = '.\\out'
     threshOF = float
+    
+    class Input:
+        dirFiles =r'D:\Datasets\Termografias\Phase-Shifted_Induction_Thermography\POD\SetAuthentic\Cache'
+        fns=[\
+            'AHT_020_FlirX6541sc_220fps_3kW_B035_j000_30kHz_100PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
+            'AHT_020_FlirX6541sc_220fps_3kW_B035_j045_30kHz_90PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
+            'AHT_020_FlirX6541sc_220fps_3kW_B035_j090_30kHz_100PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
+            'AHT_020_FlirX6541sc_220fps_3kW_B035_j135_30kHz_90PWM_MP_3P_5Hz_L_220601.ITvisLockin'\
+            ]
+        betasDeg=[0,45,90,135]
--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from globalVars import *
 def iniEnv():
     thermo_io.iniEnv()
 
-
-
-
 if __name__=='__main__':
     print('''
 --------------------------------------------
@@ -20,33 +17,11 @@ if __name__=='__main__':
 --------------------------------------------
 ''')
     iniEnv()
-
-    Params.outputDir = '.\\out'
-
-    #input
-    #   TODO serializar
-    dirThermos =r'D:\Datasets\Termografias\Phase-Shifted_Induction_Thermography\POD\SetAuthentic\Cache'
-    fns=[\
-        'AHT_020_FlirX6541sc_220fps_3kW_B035_j000_30kHz_100PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
-        'AHT_020_FlirX6541sc_220fps_3kW_B035_j045_30kHz_90PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
-        'AHT_020_FlirX6541sc_220fps_3kW_B035_j090_30kHz_100PWM_MP_3P_5Hz_L_220601.ITvisLockin',\
-        'AHT_020_FlirX6541sc_220fps_3kW_B035_j135_30kHz_90PWM_MP_3P_5Hz_L_220601.ITvisLockin'\
-        ]
-    betasDeg=[0,45,90,135]
-    dirThermos =r'D:\Datasets\Termografias\DynamicMulti_Paper\Records\2023_11\Palanquillas\90x250'
-    fns=[\
-        r'Palanquilla_90x250_Induction_20231122_203841_v375_flock5_n1_nTot288_2.ItvisLockin' \
-        ]
-    betasDeg=[0]
-        
-        
         
     phases = list[np.ndarray]()
-    for fn in fns:
-        termos, acquisitionPeriods = thermo_io.loadThermo(f'{dirThermos}\\{fn}',1,1)
+    for fn in Params.Input.fns:
+        termos, acquisitionPeriods = thermo_io.loadThermo(f'{Params.Input.dirFiles}\\{fn}',1,1)
         for t in termos:
-            cv.imwritemulti(f'{Params.outputDir}\\raw.tiff',t)
-            exit(0)
             # print(f'{str(t.shape)}')
             mag, pha = thermo_procs.FFT(t, acquisitionPeriods)
             phases.append(pha)
@@ -65,7 +40,7 @@ if __name__=='__main__':
     for phase in phases:
         imgs.append(helpers_imgs.ConvertToMaxContrastUchar(phase))
 
-    psRet = thermo_phase_shift.pixelWisePhaseShift(phases, betasDeg)
+    psRet = thermo_phase_shift.pixelWisePhaseShift(phases, Params.Input.betasDeg)
     if psRet is None:
         exit(0)
             
